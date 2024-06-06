d2200880b3d4bdb51c99b6b353dcc5c79f32dcfd
amirs-web <siami.amir4@gmail.com>
2024-06-06 11:44:57 +0330

    change vercel & app

--- app.js
+++ app.js
@@ -59,6 +59,7 @@ mongoose
   )
   .then(() => {
     app.listen(process.env.PORT || 5000);
+    console.log(`Server running on port ${process.env.PORT || 5000}`);
   })
   .catch((error) => {
     console.log(error);
